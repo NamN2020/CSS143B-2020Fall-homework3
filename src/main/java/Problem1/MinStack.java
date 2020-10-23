87e8df40a762c1736bfe6045cebc8637847c0a67
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-10-22 23:38:18 -0700

    MinStack complete

--- src/main/java/Problem1/MinStack.java
+++ src/main/java/Problem1/MinStack.java
@@ -11,26 +11,32 @@ public class MinStack extends ArrayStack<Integer> {
     // can only use Stack interface and ArrayStack from this folder
     // do not use Java Stack
     private Integer[] stack;
-    private Integer[] minStack = {Integer.MAX_VALUE};
-
+    private Integer[] min;
     int size;
+    int keepSize;
+    int minSize = 0;
 
     public MinStack(int size) {
         // homework
         super(size);
         stack = new Integer[size];
+        min = new Integer[size];
+        keepSize = size;
     }
 
     @Override
     public boolean push(Integer val) {
         // homework
-        if(minStack[0] > val){
-            minStack[0] = val;
-        }
 
         if(size < stack.length) {
             stack[size] = val;
             size++;
+            if (min[0] == null) {
+                min[0] = val;
+            } else if (stack[size -1] < min[minSize]){
+                minSize++;
+                min[minSize] = val;
+            }
             return true;
         }
         return false;
@@ -42,6 +48,14 @@ public class MinStack extends ArrayStack<Integer> {
         if(size == 0){
             System.out.println("Stack is empty");
         }
+        if(minSize == 0) {
+            min = new Integer[keepSize];
+        } else if(minSize > 0) {
+            if (stack[size - 1] == min[minSize]) {
+                minSize--;
+            }
+        }
+
         Integer val = stack[size - 1];
         size--;
         return val;
@@ -50,11 +64,11 @@ public class MinStack extends ArrayStack<Integer> {
     public Integer getMin() {
         // homework
         // loop of any kind is not allowed
-        if(minStack[0] == Integer.MAX_VALUE || minStack == null){
+        if(min.length == 0 || min == null){
             return null;
         }
 
-        return minStack[0];
+        return min[minSize];
     }
 }
 
